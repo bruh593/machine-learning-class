1ba6c169c48ef62457472e7a73efaecb1bdab9c4
bruh593 <austin.r.smith@tcu.edu>
2026-02-19 16:31:20 -0600

    Update stock tickers in extract_features function
    Added 'PLTR' to stock tickers in extract_features function.

--- src/feature_utils (1).py
+++ src/feature_utils (1).py
@@ -20,7 +20,7 @@ def extract_features():
     
     START_DATE = (datetime.date.today() - datetime.timedelta(days=365)).strftime("%Y-%m-%d")
     END_DATE = datetime.date.today().strftime("%Y-%m-%d")
-    stk_tickers = ['AAPL', 'INTL', 'AMD', 'AMZN']
+    stk_tickers = ['AAPL', 'INTL', 'AMD', 'AMZN', 'PLTR']
     ccy_tickers = ['DEXJPUS', 'DEXUSUK']
     idx_tickers = ['SP500', 'DJIA', 'VIXCLS']
     
@@ -66,5 +66,6 @@ def get_bitcoin_historical_prices(days = 60):
     df['Date'] = pd.to_datetime(df['Timestamp'], unit='ms').dt.normalize()
     df = df[['Date', 'Close Price (USD)']].set_index('Date')
     return df
+
 
 
