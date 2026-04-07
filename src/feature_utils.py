e27c9fceb689e5a1b2c38b0ae5e13e1c03d5e144
bruh593 <austin.r.smith@tcu.edu>
2026-04-07 16:34:51 -0500

    Update feature_utils.py

--- src/feature_utils.py
+++ src/feature_utils.py
@@ -95,7 +95,7 @@ def convert_input_pca_regression(request_body, request_content_type):
 
     dataset = pd.read_csv(file_path,index_col=0)
 
-    target = 'MSFT'
+    target = 'NVDA'
 
     option = 2
 
@@ -110,14 +110,14 @@ def convert_input_pca_regression(request_body, request_content_type):
 
         # Calculate the distance
         distances = np.sqrt(
-            (X[techIndicator_1] - RSI_15)**2 + 
+            (X[techIndicator_1] - RSI_10)**2 + 
             (X[techIndicator_2] - MOM_15)**2
         )
         
         closest_index = distances.idxmin()
         closest_row = X.loc[[closest_index]]
     
-        closest_row[techIndicator_1] = RSI_15
+        closest_row[techIndicator_1] = RSI_10
         closest_row[techIndicator_2] = MOM_15
     
         return closest_row
